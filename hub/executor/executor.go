682c0d97c06834a10c74525602f06aa64c3a91bf
GooRingX <gooringx@gmail.com>
2025-05-04 18:15:40 +0800

    improve

--- hub/executor/executor.go
+++ hub/executor/executor.go
@@ -209,20 +209,14 @@ func updateListeners(general *config.General, listeners map[string]C.InboundList
 
 	bindAddress := general.BindAddress
 	listener.SetBindAddress(bindAddress)
-	err = listener.ReCreateHTTP(general.Port, tunnel.Tunnel)
-	if err != nil {
-		return err
-	}
-	err = listener.ReCreateSocks(general.SocksPort, tunnel.Tunnel)
-	if err != nil {
-		return err
-	}
-	listener.ReCreateRedir(general.RedirPort, tunnel.Tunnel)
-	listener.ReCreateTProxy(general.TProxyPort, tunnel.Tunnel)
 	err = listener.ReCreateMixed(general.MixedPort, tunnel.Tunnel)
 	if err != nil {
 		return err
 	}
+	listener.ReCreateHTTP(general.Port, tunnel.Tunnel)
+	listener.ReCreateSocks(general.SocksPort, tunnel.Tunnel)
+	listener.ReCreateRedir(general.RedirPort, tunnel.Tunnel)
+	listener.ReCreateTProxy(general.TProxyPort, tunnel.Tunnel)
 	listener.ReCreateShadowSocks(general.ShadowSocksConfig, tunnel.Tunnel)
 	listener.ReCreateVmess(general.VmessConfig, tunnel.Tunnel)
 	listener.ReCreateTuic(general.TuicServer, tunnel.Tunnel)
